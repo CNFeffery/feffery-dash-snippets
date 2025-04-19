6876ec7d362a5aae9a0ff638f34a6b9c369e8801
CNFeffery <fefferypzy@gmail.com>
2025-04-19 22:52:17 +0800

    feat: 新增greens_8色彩生成指令

--- generate_snippets.py
+++ generate_snippets.py
@@ -401,6 +401,12 @@ raw_snippets = [
                 'description': 'Quickly generate greens_7 colors',
                 'body_path': r'raw_snippets\py\colors\greens_7.py',
             },
+            {
+                'title': 'greens_8 colors',
+                'prefix': 'color:greens_8',
+                'description': 'Quickly generate greens_8 colors',
+                'body_path': r'raw_snippets\py\colors\greens_8.py',
+            },
         ],
     },
 ]
